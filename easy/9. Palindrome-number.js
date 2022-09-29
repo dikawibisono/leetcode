a453a89e42770f90b16cb817a309a28b0da888c7
dikasono <97390732+dikawibisono@users.noreply.github.com>
2022-09-29 09:38:53 +0700

    add palindrome

---
+++ easy/9. Palindrome-number.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(x) {
+    
+    let strNum = x.toString()
+    
+    for (let i = 0; i < strNum.length/2; i++){
+        if (strNum[i] != strNum[strNum.length - 1 - i]) return false
+    }
+    return true
+    
+};
